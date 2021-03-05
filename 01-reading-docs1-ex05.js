ee1e8a48c036e19ea8a696cefe58103395f9402a
a green leaf <greenleaf@as-iMac.local>
2021-03-05 06:31:33 -0500

    added exercise 5

---
+++ 01-reading-docs1-ex05.js
@@ -0,0 +1,14 @@
+/*
+  What happens if we take the array ['fish', 'and', 'chips'] and try to access 
+  the element at index position 10? 
+
+  Try this in your JavaScript console.
+*/
+
+
+// the value undefined is returned
+let array = ['fish', 'and', 'chips'];
+console.log(array[10]);                 // undefined
+
+
+
