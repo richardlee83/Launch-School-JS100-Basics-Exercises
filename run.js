c8ba5e84d01759bdfe92561d2bc34f6940581a00
piano <piano@pianos-MacBook-Air.local>
2021-03-07 15:48:57 -0500

    modified run file

--- run.js
+++ run.js
@@ -8,3 +8,13 @@ for (let i = 1; i <=100; i += 1) {
     console.log('Done!');
   }
 }
+
+let char = '';
+for (let i = 1; i <=10; i += 1) {
+  char += '*';
+  let dash = '';
+  for (let j = 1; j <= 10 - char.length; j += 1) {
+    dash += '-';
+  }
+  console.log(char + dash);
+}
