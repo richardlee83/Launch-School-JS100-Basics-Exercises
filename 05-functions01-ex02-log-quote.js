3d41bf725944f5d5d65e5458c68ecdb4b4c6281e
a green leaf <greenleaf@as-iMac.local>
2021-03-13 07:34:33 -0500

    added exercise 2

---
+++ 05-functions01-ex02-log-quote.js
@@ -0,0 +1,33 @@
+/*
+  Write a function that logs Brendan Eich's quote 'Always bet on JavaScript.'
+  to the console. 
+
+  What is the return value of the function?
+
+  brendanEichQuote();
+  // logs:
+  // Always bet on JavaScript.
+*/
+
+function brendanEichQuote() {
+  console.log('Always bet on JavaScript.');
+}
+
+console.log(brendanEichQuote()); 
+// Always bet on JavaScript.
+// undefined
+
+/*
+  Solution:
+  function brendanEichQuote() {
+    console.log('Always bet on JavaScript.');
+  }
+
+  - Since the function has no return statement, the return value 
+    of our function is undefined.
+*/
+
+
+
+
+
