7faf596c45f6054c2e00eb010d7cbeb627b18928
a green leaf <greenleaf@as-iMac.local>
2021-03-17 06:50:20 -0400

    modified run.js file

--- run.js
+++ run.js
@@ -1,23 +1,3 @@
-for (let i = 1; i <= 10; i += 1) {
-  console.log(i);
-}
-
-for (let i = 1; i <=100; i += 1) {
-  console.log(i * 2);
-  if (i === 100) {
-    console.log('Done!');
-  }
-}
-
-let char = '';
-for (let i = 1; i <=10; i += 1) {
-  char += '*';
-  let dash = '';
-  for (let j = 1; j <= 10 - char.length; j += 1) {
-    dash += '-';
-  }
-  console.log(char + dash);
-}
-
-let string = 'hello'
-console.log(string.repeat(3));
+let energy = ['fossil', 'solar', 'wind', 'tidal', 'fusion'];
+energy = energy.slice(1).concat(['geothermal']);
+console.log(energy);    // [ 'solar', 'wind', 'tidal', 'fusion', 'geothermal' ]
